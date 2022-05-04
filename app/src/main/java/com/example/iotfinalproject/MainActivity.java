55bc1a49cb48ae3f487cf0389bfa1d2eae06fb37
BairnOwl <linda.chang.ace@gmail.com>
2022-05-04 04:07:46 -0500

    connected raspberry pi and android app

--- app/src/main/java/com/example/iotfinalproject/MainActivity.java
+++ app/src/main/java/com/example/iotfinalproject/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.iotfinalproject;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewReplyFromServer;
+    private EditText mEditTextSendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSend = (Button) findViewById(R.id.btn_send);
+        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
+
+        buttonSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_send:
+                sendMessage();
+                break;
+        }
+    }
+
+    private void sendMessage() {
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket("100.71.88.14", 65432);
+
+                    PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String response = in.readLine();
+
+                    System.out.println("GSR sensor value: " + response);
+
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
